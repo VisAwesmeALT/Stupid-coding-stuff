b585bc79a2dbccbb27ae519c31c0a6b90e5b0f83
VisAwesme <104176687+VisAwesme@users.noreply.github.com>
2025-02-27 08:55:44 -0500

    first version, only sends you to youtube

--- totallynotarickroll.sh
+++ totallynotarickroll.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+# Countdown from 10 to 1
+for i in {10..1}; do
+  echo "Counting down: $i"
+  sleep 1
+done
+
+# Redirect to Rickroll
+xdg-open "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
